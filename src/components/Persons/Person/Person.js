819e1e07f7eddccc06daab01552b60c3b1873f9f
Carlos Abello <andres.abello@globant.com>
2018-10-04 21:32:59 -0500

    converting stateless components into stateful components
    converting stateless components into stateful components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+
+// Stateful or presentational component is created with a es6 class
+
+class Person extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <p>I'm a {this.props.name} and I'am {this.props.age} years old</p>
+        <input type="text" onChange={this.props.changeName} value={this.props.name} />
+        <button onClick={this.props.deletePerson}>X</button>
+      </div>
+    )
+  }
+}
 
 // stateless or presentational component is always a function
 
-const Person = (props) => {
-    return (
-        <div>
-            <p>I'm a {props.name} and I'am {props.age} years old</p>
-            <input type="text" onChange={props.changeName} value={props.name} />
-            <button onClick={props.deletePerson}>X</button>
-        </div>
-    )
-}
+// const Person = (props) => {
+//     return (
+//         <div>
+//             <p>I'm a {props.name} and I'am {props.age} years old</p>
+//             <input type="text" onChange={props.changeName} value={props.name} />
+//             <button onClick={props.deletePerson}>X</button>
+//         </div>
+//     )
+// }
 
 export default Person;
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,22 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 
-const Persons = (props) => {
-  if (props.showPersons) {
-    return props.personsData.map((person, index) => {
-      return (
-        <Person
-          name={person.name}
-          age={person.age}
-          key={index}
-          changeName={(event) => props.changeNameHandler(event, index)}
-          deletePerson={() => props.deletePersonsHandler(index)}
-        />
-      )
-    });
-  } else {
-    return null;
+
+// Stateful component
+
+class Persons extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    if (this.props.showPersons) {
+      return this.props.personsData.map((person, index) => {
+        return (
+          <Person
+            name={person.name}
+            age={person.age}
+            key={index}
+            changeName={(event) => this.props.changeNameHandler(event, index)}
+            deletePerson={() => this.props.deletePersonsHandler(index)}
+          />
+        )
+      });
+    } else {
+      return null;
+    }
   }
 }
+
+// Stateless component || functional component
+
+// const Persons = (props) => {
+//   if (props.showPersons) {
+//     return props.personsData.map((person, index) => {
+//       return (
+//         <Person
+//           name={person.name}
+//           age={person.age}
+//           key={index}
+//           changeName={(event) => props.changeNameHandler(event, index)}
+//           deletePerson={() => props.deletePersonsHandler(index)}
+//         />
+//       )
+//     });
+//   } else {
+//     return null;
+//   }
+// }
 
 export default Persons;
