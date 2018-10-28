a94d8d6ba50e3f38aedd2c24a792376eb7fd8f38
Carlos Abello <andres.abello@globant.com>
2018-10-28 18:01:32 -0500

    Axios interceptors

--- src/index.js
+++ src/index.js
@@ -3,6 +3,35 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+//AXIOS INTERCEPTORS - TO INTERCEPT ANY REQUEST OR RESPONSE GLOBALLY
+
+// REQUEST INTERCEPTOR
+axios.interceptors.request.use( request => {
+    console.log(request);
+    // return the request so it can follow its path and 
+    // reach the block that is actually calling it
+    return request;
+}, error => {
+    console.log(error);
+    // return the Promise.reject so it can follow its path to 
+    //the catch block of the statement that made the request
+    return Promise.reject(error);
+});
+
+// RESPONSE INTERCEPTOR
+axios.interceptors.response.use( response => {
+    console.log(response);
+    // return the response so it can follow its path and 
+    // reach the block that is actually calling it
+    return response;
+}, error => {
+    console.log(error);
+    // return the Promise.reject so it can follow its path to 
+    // the catch block of the statement that made the call
+    return Promise.reject(error);
+});
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
