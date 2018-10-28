48d0ca29f2851ffd2f8ed484f40ff1c7e3dcbd8e
Carlos Abello <andres.abello@globant.com>
2018-10-28 11:28:44 -0500

    Using shouldCmponentUpdate  on the Modal improving performance

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 //components
 import Backdrop from '../Backdrop/Backdrop';
@@ -6,20 +6,26 @@ import Backdrop from '../Backdrop/Backdrop';
 //css
 import Classes from './Modal.css';
 
-const Modal = (props) => {
-    return(
-        <>
-            <Backdrop show={props.show} clicked={props.modalClose}/>
-            <div 
-                className={Classes.Modal}
-                style={{
-                    transform: props.show ? 'translateY(0)' : 'translatey(-100vh)',
-                    opacity: props.show ? '1': '0'
-                }}>
-                {props.children}
-            </div>
-        </>
-    );
+class Modal extends Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show;
+    }
+
+    render() {
+        return(
+            <>
+                <Backdrop show={this.props.show} clicked={this.props.modalClose}/>
+                <div 
+                    className={Classes.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translatey(-100vh)',
+                        opacity: this.props.show ? '1': '0'
+                    }}>
+                    {this.props.children}
+                </div>
+            </>
+        );
+    }
 }
 
 export default Modal;
