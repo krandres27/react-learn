545055c953802c50d378aeab213751cdaf69ec47
Carlos Abello <andres.abello@globant.com>
2018-10-28 16:54:15 -0500

    Fetching data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,16 +7,40 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            posts : [],
+            selectedPost: null
+        }
+
+        this.handleSelectedPost = this.handleSelectedPost.bind(this);
+    }
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts').then((res) => {
+            const fetchedPosts = res.data.slice(0,4);
+            this.setState({posts: fetchedPosts});
+        });
+    }
+
+    handleSelectedPost(id) {
+        this.setState({selectedPost: id});
+    }
+
     render () {
+        const posts = this.state.posts.map( post => {
+            return <Post key={post.id} title={post.title} clicked={ () => this.handleSelectedPost(post.id) }/>
+        });
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost postId={this.state.selectedPost}/>
                 </section>
                 <section>
                     <NewPost />
