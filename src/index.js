a054615f191a8571f75586abc09073c95582d0cc
Carlos Abello <andres.abello@globant.com>
2018-10-30 15:40:39 -0500

    Adding enhancers and config to user dev tools

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
@@ -24,8 +24,11 @@ const logger = store => {
     }
 }
 
+// DEBUGGIN
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 // THE STORE SHOULD BE CREATED BEFORE OR WHEN THE APPLICATION START
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger)));
 
 // PROVIDER IS A HELPER COMPONENT WHICH ALLOWS TO INJECT THE STORE ON THE REACT COMPONENTS
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
