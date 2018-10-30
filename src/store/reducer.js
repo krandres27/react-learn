a8deff641424770799ea605ba6ba71ea4a406fcc
Carlos Abello <andres.abello@globant.com>
2018-10-30 11:24:36 -0500

    Setting up the reducers to add and delete persons

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -5,6 +5,29 @@ const initialState = {
 
 // reducer
 const reducer = (state = initialState, action) => {
+
+    if(action.type === 'ADD_PERSON') {
+        const newPerson = {
+            id: Math.random(), // not really unique but good enough here!
+            name: 'Max',
+            age: Math.floor( Math.random() * 40 )
+        }
+
+        return {
+            ...state,
+            persons: state.persons.concat(newPerson)
+        }
+    }
+    
+    if(action.type === 'DELETE_PERSON') {
+        const updatedPersonsArray = action.persons.filter( person => person.id !== action.personId);
+
+        return {
+            ...state,
+            persons: updatedPersonsArray
+        }
+    }
+
     return state;
 }
 
