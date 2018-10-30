4e36b8e2c85600ec8eae515498d2d1cfe4803f5c
Carlos Abello <andres.abello@globant.com>
2018-10-29 22:19:34 -0500

    Adding Store and Reducer to the app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+// THE REDUCER THAT THE CREATE METHOD NEEDS
+import reducer from './store/reducer';
+
+// THE STORE SHOULD BE CREATED BEFORE OR WHEN THE APPLICATION START
+const store = createStore(reducer);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
